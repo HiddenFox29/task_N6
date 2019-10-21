16f9bc3cf07bdda883e88eb4cfc480277f57cd64
Aleksandr Alekseev <hiddenfox29@yandex.ru>
2019-10-21 20:08:17 +0600

    2 com

--- sale.js
+++ sale.js
@@ -1,12 +1,16 @@
-let price = [15000, 10000, 1000, 20000 ]
-let items = ['стол', 'стул', 'зеркало', 'палатенцо']
-let li = document.createElement("li");
+let prices = [15000, 10000, 1000, 20000];
+let items = ["Table", "Chair", "Towel", "Mirror"];
+let i = 0;
+let discount = 0.3; // 30% discount
 
-for (i = 0; i < price.length; i++){
-    document.getElementsByTagName("li").innerText = price[i];
 
+document.write ("<h1>", "Old Prices", "<br>", "</h1>")
+for (i = 0; i < prices.length; i++) {
+    document.write ("<li>", items[i] + " " + prices[i], "</li>", "<br>");
 }
-  // let li = document.createElement("li");
-  // let text = document.createTextNode(price[i]);
-  // li.appendChild(text);
-  // document.getElementById("myUl").appendChild(li);
+
+
+document.write ("<h1>", "New Prices", "<br>", "</h1>")
+for (i = 0; i < prices.length; i++) {
+    document.write ("<li>", items[i] + " " + (prices[i] - (prices[i] * discount)), "</li>", "<br>");
+}
